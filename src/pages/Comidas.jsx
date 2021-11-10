ed8b0c0fd8f2af08b44ec5242d96b4608999aa52
WalmirLucena <walmirlucena3@gmail.com>
2021-11-10 10:11:12 -0400

    Adiciona Footer

--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import FoodSearchResults from '../components/FoodSearchResults';
+import Footer from '../components/Footer';
 
 export default function Comidas() {
   return (
     <>
       <Header title="Comidas" profile search />
       <FoodSearchResults />
+      <Footer />
     </>
   );
 }
